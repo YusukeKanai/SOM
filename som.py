2e0fd4d798d47f2e6238bff6e4a5d08d26688dfc
Yusuke Kanai <yusuke.kanai.gh@gmail.com>
2018-07-05 01:55:11 +0900

    サンプル作った

--- som.py
+++ som.py
@@ -11,6 +11,7 @@ from __future__ import print_function, unicode_literals, division
 from sklearn import decomposition
 import matplotlib.pyplot as plt
 from chainer.datasets import mnist
+import numpy as np
 
 
 class PCA(object):
@@ -35,58 +36,93 @@ class PCA(object):
 
 
 class SelfOrganizationMap(object):
-    def __init__(self, data, decomposer=None):
+    def __init__(self, dim, data, epoch=1, decomposer=None):
+        self.dim = dim
         self.original_data = data
 
         self.decomposited = False
         if decomposer:
             self.decomposited = True
             self.data = decomposer.fit(data)
+        else:
+            self.data = data
+
+        dim_size = len(self.data[0])
+        self.node = np.random.rand(dim * dim, dim_size)
+        self.winner = None
+
+        self.targetId = 0
+        self.epoch = epoch
+
+    def __iter__(self):
+        return self
 
     def __next__(self):
-        pass
+        if self.epoch == 0:
+            raise StopIteration()
 
-    def winner_takes_all(self):
-        pass
+        target_neuron = self.data[self.targetId]
+        self.set_winner(target_neuron)
+        h = self.func_neighborhood()
+        self.update(h)
+
+        if self.targetId == len(self.data) - 1:
+            self.targetId = 0
+            self.epoch -= 1
+
+            if self.epoch == 0:
+                raise StopIteration()
+
+        else:
+            self.targetId += 1
+
+        # get nearliest image
+        image_node_map = dict()
+        for i in range(len(self.data)):
+            nid, ref = self.nearliest_node(self.data[i])
+            if nid in image_node_map:
+                _, ref2 = image_node_map[nid]
+                if ref2 < ref:
+                    continue
+            image_node_map[nid] = (i, ref)
+
+        # set image to node
+        res = np.zeros((self.dim ** 2, 784))
+        for k, v in image_node_map.items():
+            res[k] = self.original_data[v[0]]
+
+        return res
+
+    def nearliest_node(self, src):
+        z = self.node - src
+        ref = np.sum(z ** 2, axis=1)
+        return np.argmax(ref), np.max(ref)
+
+    def set_winner(self, neuron):
+        self.winner, _ = self.nearliest_node(neuron)
 
     def func_neighborhood(self):
-        pass
+        dists = (self.node - self.node[self.winner]) ** 2
+        return np.exp(-dists / (self.dim ** 2))
 
-    def update(self):
-        pass
-
-
-class Drawer(object):
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-
-        self.fig = plt.figure()
-
-    def print(self, dataset):
-        """
-        :param data: [png, (point)]
-        :return:
-        """
-        for data in dataset:
-            img, pos = data
-            subfig = self.fig.add_subplot(self.height, self.width, 10 * pos[0] + pos[1] + 1)
-            subfig.get_xaxis().set_visible(False)
-            subfig.get_yaxis().set_visible(False)
-            plt.imshow(img)
-
-        plt.show()
+    def update(self, h):
+        self.node = self.node + 0.05 * h * self.data - self.node[self.winner]
 
 
 if __name__ == '__main__':
-    dataset = mnist.get_mnist(withlabel=False, ndim=2)[0][:10000]
+
+    print("データセットの用意")
+    dataset = mnist.get_mnist(withlabel=False, ndim=1)[0][:10000]
 
     sample = dataset[:100]
-    sample_data = []
 
-    for i in range(10):
-        for j in range(10):
-            sample_data.append([sample[10 * i + j], (i, j)])
+    print("SOMの準備")
 
-    drawer = Drawer(10, 10)
-    drawer.print(sample_data)
+    som = SelfOrganizationMap(10, sample, 1, PCA(dim=20))
+
+    print("イタレータ実行")
+    for sample in som:
+        sample = sample.reshape(10, 10, 28, 28)
+        sample = sample.transpose(0, 2, 1, 3).reshape((10 * 28, 10 * 28))
+        plt.imshow(sample)
+        plt.show()
