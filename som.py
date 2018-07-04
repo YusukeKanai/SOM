5465633f8309f2b3b02243313f2073948631eace
Yusuke Kanai <yusuke.kanai.gh@gmail.com>
2018-07-04 14:10:28 +0900

    主成分分析によるデータ圧縮を適応

--- som.py
+++ som.py
@@ -12,18 +12,34 @@ from sklearn import decomposition
 
 
 class PCA(object):
+    """
+    主成分分析で次元圧縮
+    """
+
     def __init__(self, dim):
+        """
+        :param dim: 次元数
+        """
         self.dim = dim
         self.pca = decomposition.PCA(n_components=self.dim)
 
     def fit(self, data):
+        """
+        :param data: 元データ
+        :return: 圧縮後のデータ
+        """
         self.pca.fit(data)
         return self.pca.transform(data)
 
 
 class SelfOrganizationMap(object):
     def __init__(self, data, decomposer=None):
-        pass
+        self.original_data = data
+
+        self.decomposited = False
+        if decomposer:
+            self.decomposited = True
+            self.data = decomposer.fit(data)
 
     def __next__(self):
         pass
