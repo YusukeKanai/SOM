ebe58f88276059b7baddf5387a0c088cadedcb70
Yusuke Kanai <yusuke.kanai.gh@gmail.com>
2018-07-05 07:17:25 +0900

    修正した

--- som.py
+++ som.py
@@ -36,35 +36,31 @@ class PCA(object):
 
 
 class SelfOrganizationMap(object):
-    def __init__(self, dim, data, epoch=1, decomposer=None):
+    def __init__(self, dim, data, epoch=1, decompose=None):
         self.dim = dim
         self.original_data = data
 
-        self.decomposited = False
-        if decomposer:
-            self.decomposited = True
-            self.data = decomposer.fit(data)
+        if decompose:
+            self.data = decompose.fit(data)
         else:
             self.data = data
 
-        dim_size = len(self.data[0])
-        self.node = np.random.rand(dim * dim, dim_size)
+        data_size = len(self.data[0])
+        self.node = np.random.normal(size=(dim ** 2, data_size))
         self.winner = None
 
         self.targetId = 0
         self.epoch = epoch
+        self.rate = 1.0
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.epoch == 0:
-            raise StopIteration()
-
         target_neuron = self.data[self.targetId]
         self.set_winner(target_neuron)
         h = self.func_neighborhood()
-        self.update(h)
+        self.update(target_neuron, h)
 
         if self.targetId == len(self.data) - 1:
             self.targetId = 0
@@ -75,6 +71,7 @@ class SelfOrganizationMap(object):
 
         else:
             self.targetId += 1
+        self.rate *= 0.9
 
         # get nearliest image
         image_node_map = dict()
@@ -96,33 +93,39 @@ class SelfOrganizationMap(object):
     def nearliest_node(self, src):
         z = self.node - src
         ref = np.sum(z ** 2, axis=1)
-        return np.argmax(ref), np.max(ref)
+        return np.argmin(ref), np.min(ref)
 
     def set_winner(self, neuron):
         self.winner, _ = self.nearliest_node(neuron)
 
     def func_neighborhood(self):
-        dists = (self.node - self.node[self.winner]) ** 2
-        return np.exp(-dists / (self.dim ** 2))
+        wi = self.winner // self.dim
+        wj = self.winner % self.dim
+        dists = np.zeros((self.dim ** 2, 1))
+        for i in range(self.dim):
+            for j in range(self.dim):
+                dists[i * self.dim + j] = (wi - i) ** 2 + (wj - j) ** 2
+        return np.exp(-dists / float(self.dim))
 
-    def update(self, h):
-        self.node = self.node + 0.05 * h * self.data - self.node[self.winner]
+    def update(self, src, h):
+        self.node = self.node + self.rate * h * (src - self.node)
 
 
 if __name__ == '__main__':
+    map_dim = 20
 
     print("データセットの用意")
-    dataset = mnist.get_mnist(withlabel=False, ndim=1)[0][:10000]
-
-    sample = dataset[:100]
+    dataset = mnist.get_mnist(withlabel=False, ndim=1)[0][:20000]
 
     print("SOMの準備")
-
-    som = SelfOrganizationMap(10, sample, 1, PCA(dim=20))
+    som = SelfOrganizationMap(map_dim, dataset, 2, PCA(dim=50))
 
     print("イタレータ実行")
-    for sample in som:
-        sample = sample.reshape(10, 10, 28, 28)
-        sample = sample.transpose(0, 2, 1, 3).reshape((10 * 28, 10 * 28))
+    for i, sample in enumerate(som):
+        if i % 4000 != 0:
+            continue
+
+        sample = sample.reshape(map_dim, map_dim, 28, 28)
+        sample = sample.transpose(0, 2, 1, 3).reshape((map_dim * 28, map_dim * 28))
         plt.imshow(sample)
-        plt.show()
+        plt.savefig("sample_{}".format(i))
